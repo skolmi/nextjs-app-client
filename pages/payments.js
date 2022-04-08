02540dba6ad84728fdc5b6347cefa728f5fc8041
skolmiDev <programador01@gmail.com>
2022-04-08 12:25:29 -0500

    error in epayco button

--- pages/payments.js
+++ pages/payments.js
@@ -1,5 +1,6 @@
 import Layout from "../components/layout";
 import ScriptEpaycoButtonCheckout from "../components/ScriptEpaycoButtonCheckout";
+import Script from "next/script";
 
 export default function payments() {
     // var ePayco = {
@@ -21,8 +22,25 @@ export default function payments() {
 
     return (
         <Layout>
+            <form>
+                <Script
+                    src="https://checkout.epayco.co/checkout.js"
+                    class="epayco-button"
+                    data-epayco-key="e2a4be24af5151166c2d45c1fcb1214c"
+                    data-epayco-amount="50000"
+                    data-epayco-name="Vestido Mujer Primavera"
+                    data-epayco-description="Vestido Mujer Primavera"
+                    data-epayco-currency="cop"
+                    data-epayco-country="co"
+                    data-epayco-test="true"
+                    data-epayco-external="false"
+                    data-epayco-response="https://ejemplo.com/respuesta.html"
+                    data-epayco-confirmation="https://ejemplo.com/confirmacion"
+                    data-epayco-methodconfirmation="get"
+                />
+            </form>
             <button type="button">Oprimeme</button>
-            <ScriptEpaycoButtonCheckout/>
+            <ScriptEpaycoButtonCheckout />
         </Layout>
     );
 }
