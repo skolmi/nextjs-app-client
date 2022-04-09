717cb178631fc4f77b56445fa91e63b8cdf8d3ac
skolmiDev <programador01@gmail.com>
2022-04-09 07:40:54 -0500

    test manual review aws pipeline

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout';
 export default function Index() {
   return (
     <Layout>
-
+      <p>Este es el test de la verificacion manual en code pipeline</p>
     </Layout>
   )
 }
