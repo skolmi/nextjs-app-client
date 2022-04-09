c3b085eb90be06a52cbca5d390bef62503a463e0
ByronCarrion <carrionmejia756@gmail.com>
2022-04-09 08:15:52 -0600

    prueba

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ export default function Index() {
   return (
     <Layout>
       <p>Este es el test de la verificacion manual en code pipeline</p>
+
+      <p>Soy Byron Haciendo un test</p>
     </Layout>
   )
 }
