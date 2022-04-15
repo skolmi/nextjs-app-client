171e1e1ea8b354e99a6257628bca69ebf770250f
skolmiDev <programador01@gmail.com>
2022-04-15 00:21:09 -0500

    component buttonform is ok

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,11 +1,14 @@
 import Navbar from './../Navbar';
 import style from './layout.module.css'
+import Script from 'next/script';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Layout({ children }) {
     return (
         <>
             <Navbar />
             <main className={style.main}>{children}</main>
+            <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></Script>
         </>
     )
 }
