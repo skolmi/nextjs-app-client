f85426dc162563a2a282e64d2e3719eb3c013790
skolmiDev <programador01@gmail.com>
2022-07-12 11:44:49 -0500

    agregando pagina de inicio como respuesta a una transaccion epayco

---
+++ pages/response.js
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import Layout from "../components/layout";
+
+
+export default function response () {
+    return(
+        <Layout>
+            <Box>
+                aca estoy
+            </Box>
+        </Layout>
+    );
+}
